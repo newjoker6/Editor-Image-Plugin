0fcfca9d4b078bfe4627e0361caca5e03a17044e
Michael McGuire <newjoker6@gmail.com>
2022-06-09 02:51:28 -0700

    Added alert

--- addons/EditorImage/EditorImage.gd
+++ addons/EditorImage/EditorImage.gd
@@ -1,52 +1,56 @@
 tool
 extends EditorPlugin
 
-var base
-var ScriptText
-var ScriptTextEditor2
-var tabContainer
 var open_scripts:Array
+var wait_time_between_script_checks := 3.0
+const script_child_size_no_background := 7
+
+var base
+var tabContainer
+var vbox630
+
 
 func _enter_tree() -> void:
-	base = get_editor_interface().get_base_control()
-	var vbox630: VBoxContainer = base.get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0)
-	ScriptTextEditor2 = vbox630.get_child(1).get_child(0).get_child(2).get_child(0).get_child(1).get_child(1).get_child(0)
-	ScriptText = ScriptTextEditor2.get_child(0).get_child(0).get_child(0)
-	tabContainer = vbox630.get_child(1).get_child(0).get_child(2).get_child(0).get_child(1).get_child(1)
+	print('SAVE ANY SCRIPT TO FIX ERROR')
+	
+	OS.alert('Open and save any GDScript script to resolve loading error. ' \
+	+ "This error occurs as the necessary parts of the editor aren't loaded until you do. " \
+	+ 'I have found no way to delay this portion of the code that actually works.', 'Editor Image')
 	
 	var timer = Timer.new()
-	timer.wait_time = 5.0
+	timer.wait_time = wait_time_between_script_checks
 	timer.set_autostart(true)
 	self.add_child(timer)
 	timer.connect("timeout", self, 'on_timeout')
 
-#	add_image(ScriptText)
+	call_deferred('check_scripts')
 
-#	match_rect(text_rect, ScriptText)
 
-#
 func match_rect(of: Control, to: Control) -> void:
 	of.rect_min_size = to.rect_min_size
 	of.rect_size = to.rect_size
 	of.rect_global_position = to.rect_global_position
 	
+	
 func _exit_tree():
 	queue_free()
 
+
 func on_timeout():
+	check_scripts()
 	open_scripts.clear()
 	
-	for s in tabContainer.get_children():
-		if 'ScriptTextEditor' in s.name:
-			
-			var scriptedit = tabContainer.get_node(str(s.name)).get_child(0).get_child(0).get_child(0)
-			open_scripts.append(scriptedit.name)
+	if not typeof(tabContainer) == 0:
+		for s in tabContainer.get_children():
+			if 'ScriptTextEditor' in s.name:
+				var scriptedit = tabContainer.get_node(str(s.name)).get_child(0).get_child(0).get_child(0)
+				open_scripts.append(scriptedit.name)
 
-			if scriptedit.get_children().size() < 7:
-				add_image(scriptedit)
+				if scriptedit.get_children().size() < script_child_size_no_background:
+					add_image(scriptedit)
 
 
-func add_image(code):
+func add_image(code) -> void:
 	var texture_rect := TextureRect.new()
 	texture_rect.texture = preload("res://addons/EditorImage/Background.png")
 	texture_rect.expand = true
@@ -58,5 +62,17 @@ func add_image(code):
 	code.add_child(texture_rect)
 	code.move_child(texture_rect, 0)
 	code.connect("resized", self, "match_rect", [ texture_rect, code ])
-	match_rect(texture_rect, ScriptText)
+	match_rect(texture_rect, code)
+
+
+func check_scripts() -> void:
+	base = get_editor_interface().get_base_control()
+	vbox630= base.get_child(0).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0)
+
+	var ScriptTextEditor2 = vbox630.get_child(1).get_child(0).get_child(2).get_child(0).get_child(1).get_child(1).get_child(0)
+	var ScriptText = ScriptTextEditor2.get_child(0).get_child(0).get_child(0)
+	tabContainer = vbox630.get_child(1).get_child(0).get_child(2).get_child(0).get_child(1).get_child(1)
+
+
+
 
